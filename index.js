e46996413c4366f10b344ca98af70c8fc853b385
Samantha Crespo <samkcrespo@gmail.com>
2022-04-07 13:26:29 -0700

    commit tree e849fdb3f4ec7c58dd681631da2cd576bfe746e1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (newEmployee, key, value) {
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newObj = {...object}
+    delete newObj[key]
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object;
+}
+
 // Write your solution in this file!
